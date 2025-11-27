fc245b5500a5c12ad4ab2ca69c7b45e54b00bd62
RafaelPereirax <rafael.pereiras@outlook.com>
2025-11-27 11:39:37 -0300

    Commit 12

--- src/fag/Movie.java
+++ src/fag/Movie.java
@@ -2,6 +2,7 @@ package fag;
 
 public class Movie {
 
+	  // Constantes de preço (ainda necessárias para o Price.createPrice())
 	  public static final int  CHILDRENS = 2;
 	  public static final int  REGULAR = 0;
 	  public static final int  NEW_RELEASE = 1;
@@ -9,20 +10,13 @@ public class Movie {
 	  private String _title;
 	  private Price _price; // Usa a nova classe Price
 
-	  // Construtor: usa o setter que chama o Factory Method
+	  // Construtor: AGORA CHAMA O FACTORY METHOD DIRETAMENTE (MUDANÇA DO COMMIT 12)
 	  public Movie(String title, int priceCode) {
 	      _title = title;
-	      setPriceCode(priceCode); // Chama o setter (que usa createPrice)
+	      _price = Price.createPrice(priceCode); 
 	  }
       
-      // getPriceCode() FOI REMOVIDO! (Será removido no Commit 12, mas agora o mantemos para compilar)
-      // Como o método foi removido de Price, o melhor é removê-lo de Movie também.
-      // Neste estágio, o getPriceCode() deve ser removido ou o código que o chama deve ser removido.
-      
-      // MANTEMOS SOMENTE setPriceCode
-	  public void setPriceCode(int arg) {
-	      _price = Price.createPrice(arg);
-	  }
+      // getPriceCode() e setPriceCode() FORAM REMOVIDOS! (MUDANÇA DO COMMIT 12)
 
 	  public String getTitle (){
 	      return _title;
