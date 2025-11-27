bf45a0b9ffa1cbeb0aa19664aeb67589b17cf59c
RafaelPereirax <rafael.pereiras@outlook.com>
2025-11-27 10:45:58 -0300

    Commit 7

--- src/fag/Movie.java
+++ src/fag/Movie.java
@@ -25,31 +25,39 @@ public class Movie {
 	  public String getTitle (){
 	      return _title;
 	  }
-	// Adicione este método dentro da classe Movie.java
-	  public double getCharge(int daysRented) {
-	      double result = 0;
-	      switch (getPriceCode()) {
-	          case Movie.REGULAR:
-	              result += 2;
-	              if (daysRented > 2)
-	                  result += (daysRented - 2) * 1.5;
-	              break;
-	          case Movie.NEW_RELEASE:
-	              result += daysRented * 3;
-	              break;
-	          case Movie.CHILDRENS:
-	              result += 1.5;
-	              if (daysRented > 3)
-	                  result += (daysRented - 3) * 1.5;
-	              break;
-	      }
-	      return result;
-	  }
-	// Adicione este método dentro da classe Movie.java
+      
+    // MÉTODO getCharge() SIMPLIFICADO (MUDANÇA DO COMMIT 7)
+    public double getCharge(int daysRented) {
+        // Agora, ele apenas chama o novo método auxiliar
+        return getThisAmount(daysRented);
+    }
+    
+    // NOVO MÉTODO AUXILIAR getThisAmount() (ADICIONADO NO COMMIT 7)
+    private double getThisAmount(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+    
+    // MÉTODO DE PONTOS INALTERADO
 	  public int getFrequentRenterPoints(int daysRented) {
 	      if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
 	          return 2;
 	      }
 	      return 1;
 	  }
-	}
+}
